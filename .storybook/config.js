8eb7573469bc6790f487793d62c191fc3628ba62
Mathew Cormier <mathew.corm@gmail.com>
2018-08-14 15:49:14 -0400

    Add EthAddress

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from '@storybook/react'
 
 function loadStories() {
-  //require('../stories/button.js')
+  require('../stories/button.js')
+  require('../stories/eth-address.js')
 }
 
 configure(loadStories, module)
---
+++ stories/eth-address.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { EthAddress } from "../src/components/eth-address";
+
+storiesOf("EthAddress", module).add("with text", () => (
+  <EthAddress ethAddress="0xb4124ceb3451635dacedd11767f004d8a28c6ee7" />
+));
