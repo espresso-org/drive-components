66304083cd82f5ad4f9c0aab3ce7e1dbd3745ad9
Maxime Cote <maxime.cote184@gmail.com>
2018-08-18 17:30:22 -0400

    Add comments to aragonStoriesOf

--- src/utils/aragon-stories-of.js
+++ src/utils/aragon-stories-of.js
@@ -3,6 +3,13 @@ import { storiesOf } from "@storybook/react"
 import { AragonApp } from '@aragon/ui'
 
 
+/**
+ * Same as storiesOf but wraps the story inside an 
+ * AragonApp component
+ * 
+ * @param {string} componentName 
+ * @param {*} moduleRef 
+ */
 export function aragonStoriesOf(componentName, moduleRef) {
     let aragonStories = storiesOf(componentName, moduleRef)
 
