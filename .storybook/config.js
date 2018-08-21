c1c23f72524492d990305968c82ec69c18d98bcf
Maxime Cote <maxime.cote184@gmail.com>
2018-08-20 23:49:16 -0400

    Add SidePanel story

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,6 +4,7 @@ function loadStories() {
   require('../stories/eth-address.js')
   require('../stories/file-input.js')
   require('../stories/file-row.js')
+  require('../stories/side-panel.js')
 }
 
 configure(loadStories, module)
