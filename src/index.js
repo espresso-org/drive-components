434dfba020faa7ddf35b56fa50de1cd905bcb872
juslar <justinlaroche95@gmail.com>
2018-08-24 13:15:51 -0400

    Moved configuration section components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 export * from './components/eth-address'
 export * from './components/file-input'
 export * from './components/file-row'
+export * from './components/configuration-radio-grp'
+export * from './components/configuration-modal'
 
 export * from './utils'
