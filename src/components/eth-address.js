9f605857661a3067a9af86e42d8c433a0f90cf58
juslar <justinlaroche95@gmail.com>
2018-08-28 11:11:52 -0400

    Modify EthAddress component with Badge

--- src/components/eth-address.js
+++ src/components/eth-address.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Badge } from '@aragon/ui'
 
 export const EthAddress = ({ ethAddress }) => 
-  <div title={ethAddress}>
-    {ethAddress && `${ethAddress.substr(0, 6)}...${ethAddress.substr(ethAddress.length - 4)}`}
+  <div>
+    <IdentifierWrapper>
+      <Identifier title={ethAddress}>{ethAddress && `${ethAddress.substr(0, 6)}...${ethAddress.substr(ethAddress.length - 4)}`}</Identifier>
+    </IdentifierWrapper>
   </div>
+
+const IdentifierWrapper = styled.div`
+  max-width: 100%;
+`
+
+const Identifier = styled(Badge.App)`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: block;
+  font-size: 15px;
+  margin-left: -9px;
+`
