083e906a9429253683da69ed1b2d9a0e4e38d83c
Mathew Cormier <mathew.corm@gmail.com>
2018-09-02 15:47:47 -0400

    Remove unused css

--- src/components/configuration-radio-grp.js
+++ src/components/configuration-radio-grp.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
 import { observer } from 'mobx-react'
 import '../css/styles.css'
 
 const ConfigurationRadioBtn = observer(({ isChecked, value, index, handler}) =>
-    <span onClick={() => handler(index)}>
-        <span className={isChecked ? value + "checked radiobtn" : value + "unchecked radiobtn"} data-value={value}></span>
-    </span>
+    <RadioBtn checked={isChecked} onClick={() => handler(index)}>
+        <img 
+            data-value={value} 
+            src={require(`./configuration-modal/img/${value}-logo.png`)}    
+        />
+    </RadioBtn>
 )
 
 
@@ -23,3 +27,22 @@ export const ConfigurationRadioGrp = observer(({ options, store }) =>
         )
     }</div>
 )
+
+
+const RadioBtn = styled.div`
+    display: inline-block;
+    width: auto;
+    height: auto;
+    transition: opacity 0.3s;
+    opacity: ${({ checked }) => checked ? 1 : 0.5};
+    margin: 20px 20px 20px 40px;
+    cursor: pointer;
+
+    &:hover {
+        opacity: 1;
+    }
+
+    > img {
+        width: 80px;
+    }
+`
