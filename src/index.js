45febb727bc1786bdb1e516d9bc04e72d4608320
Maxime Cote <maxime.cote184@gmail.com>
2018-09-03 10:56:42 -0400

    Fix configuration-modal export

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export * from './components/file-row'
 export * from './components/edit-permissions'
 export * from './components/edit-panel'
 export * from './components/side-bar'
-export * from './components/configuration-modal'
+export * from './components/configuration-modal/configuration-modal'
 
 export * from './components/app'
 
