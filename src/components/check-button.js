ddb918c67854783736c3361b910497f97d5b191e
Mathew Cormier <mathew.corm@gmail.com>
2018-09-10 07:49:46 -0400

    Add check mark to checkbutton

--- src/components/check-button.js
+++ src/components/check-button.js
@@ -11,13 +11,12 @@ export const CheckButton = styled.input.attrs({ type: 'radio' })`
   margin: 5px;
   background: #f3f9fb;
   border: 1px solid #daeaef;
-  border-radius: 50%;
+  border-radius: 0;
   outline: 0;
   cursor: pointer;
   &:after {
     content: '';
     position: absolute;
-    background: #daeaef;
     width: 8px;
     height: 8px;
     border-radius: 4px;
@@ -26,17 +25,18 @@ export const CheckButton = styled.input.attrs({ type: 'radio' })`
     transition: all 100ms ease-in-out;
   }
   &:active {
-    border-color: #c9d9de;
   }
   &:active:after {
     opacity: 1;
     transform: scale(0.6);
-    background: #daeaef;
   }
   &:checked:after {
     opacity: 1;
     transform: scale(1);
-    background: #1dd9d5;
+    content: '🗸';
+    color: #1dd9d5;
+    font-size: 20px;
+    top: -7px;
   }
 `
 
