dd9b5e00a2fa33f673c68b510aea77d97e36699e
Mathew Cormier <mathew.corm@gmail.com>
2018-09-14 08:36:39 -0400

    Convert EditPermissions to functional component

--- src/components/edit-permissions/edit-permissions.js
+++ src/components/edit-permissions/edit-permissions.js
@@ -1,31 +1,21 @@
 import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react'
-import { observe } from 'mobx'
 
 import { TableRow, TableHeader, TableCell, SidePanel } from '@aragon/ui'
 import { CheckButton } from '../check-button'
 
-import { EditMode } from '../../stores/edit-mode'
 import { PermissionType } from '../../stores/permissions-store'
 
 import { EthAddress } from '../eth-address'
 import { s } from './edit-permissions.styles'
 
-@inject("mainStore", "datastore", "permissionsStore")
-@observer
-export class EditPermissions extends Component {
-
-  get permissionsStore() { return this.props.permissionsStore }
-  get mainStore() { return this.props.mainStore }
-  get file() { return this.mainStore.selectedFile }
-
-
-  render() {
-    return (
+export const EditPermissions = 
+inject("mainStore", "permissionsStore")(
+observer(({ mainStore, permissionsStore }) =>
       <s.Main>
         <s.TopButtons>
-          <s.AddButton onClick={() => this.mainStore.isAddPermissionPanelOpen = true }>Add</s.AddButton>
-          <s.RemoveButton onClick={() => this.permissionsStore.removeSelectedPermission()}>Remove</s.RemoveButton>
+          <s.AddButton onClick={() => mainStore.isAddPermissionPanelOpen = true }>Add</s.AddButton>
+          <s.RemoveButton onClick={() => permissionsStore.removeSelectedPermission()}>Remove</s.RemoveButton>
         </s.TopButtons>
         <s.AddressList 
           header={
@@ -35,27 +25,25 @@ export class EditPermissions extends Component {
               <TableHeader title="Write" />
             </TableRow>
         }>
-          {this.permissionsStore.selectedFilePermissions
+          {permissionsStore.selectedFilePermissions
             .map(permission => 
               <PermissionRow
                 key={permission.entity || permission.groupId}
                 permission={permission} 
-                onChange={permission => this.permissionsStore.updateSelectedFilePermissions(permission)}
-                selected={this.permissionsStore.isPermissionSelected(permission)}
-                onClick={() => this.permissionsStore.selectPermission(permission)}
+                onChange={permission => permissionsStore.updateSelectedFilePermissions(permission)}
+                selected={permissionsStore.isPermissionSelected(permission)}
+                onClick={() => permissionsStore.selectPermission(permission)}
               />
           )}
       
         </s.AddressList>
 
         <s.Actions>            
-          <s.SaveButton onClick={() => this.permissionsStore.savePermissionChanges() }>Save</s.SaveButton>
+          <s.SaveButton onClick={() => permissionsStore.savePermissionChanges() }>Save</s.SaveButton>
         </s.Actions>        
 
       </s.Main>
-    )
-  }
-}
+))
 
 
 
