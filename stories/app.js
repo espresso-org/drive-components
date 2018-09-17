5ca671b09e76052c689e8d12c4bf8187a929af40
Mathew Cormier <mathew.corm@gmail.com>
2018-09-16 22:01:54 -0400

    Add main app story without config

--- stories/app.js
+++ stories/app.js
@@ -11,18 +11,39 @@ import { PermissionsStore } from '../src/stores/permissions-store'
 
 
 
-const datastore = new MockedDatastore({})
-const configStore = new ConfigStore(datastore)
-const mainStore = new MainStore(datastore)
-const permissionsStore = new PermissionsStore(datastore, mainStore)
 
-datastore.createGroup("Group #1")
-datastore.createGroup("Lggkiwfj aef")
-datastore.createGroup("Group #32")
+aragonStoriesOf("Main App", module).add("Basic", () => {
+  const datastore = new MockedDatastore({})
+  const configStore = new ConfigStore(datastore)
+  const mainStore = new MainStore(datastore)
+  const permissionsStore = new PermissionsStore(datastore, mainStore)
+  
+  datastore.createGroup("Group #1")
+  datastore.createGroup("Lggkiwfj aef")
+  datastore.createGroup("Group #32")
 
+  return (
+    <Provider permissionsStore={permissionsStore} datastore={datastore} mainStore={mainStore} configStore={configStore}>
+      <App></App>
+    </Provider>
+  )
+})
 
-aragonStoriesOf("Main App", module).add("Basic", () => (
-  <Provider permissionsStore={permissionsStore} datastore={datastore} mainStore={mainStore} configStore={configStore}>
-    <App></App>
-  </Provider>
-))
+aragonStoriesOf("Main App", module).add("Without config", () => {
+  const datastore = new MockedDatastore({})
+  const configStore = new ConfigStore(datastore)
+  const mainStore = new MainStore(datastore)
+  const permissionsStore = new PermissionsStore(datastore, mainStore)
+  
+  datastore.setIpfsStorageSettings('127.0.0.1', 5001, 'http')
+
+  datastore.createGroup("Group #1")
+  datastore.createGroup("Lggkiwfj aef")
+  datastore.createGroup("Group #32")
+
+  return (
+    <Provider permissionsStore={permissionsStore} datastore={datastore} mainStore={mainStore} configStore={configStore}>
+      <App></App>
+    </Provider>
+  )
+})
