72e1621da81e9ab57251e7ca63289e0491cb3d9f
Mathew Cormier <mathew.corm@gmail.com>
2018-09-25 08:41:46 -0400

    Disabled ActionButton

--- src/components/action-button.js
+++ src/components/action-button.js
@@ -6,5 +6,11 @@ export const ActionButton = styled(Button).attrs({ mode: 'outline'})`
     display: block;
     width: 100%;
     margin: 8px 0;
-    background-color: white;
+    background-color: ${({ disabled }) => disabled ? 'transparent' : 'white'};
+    border-color: ${({ disabled }) => disabled ? '#f1ecec' : '#E6E6E6'};
+    color: ${({ disabled }) => disabled ? '#aaa' : '#707070'};
+    
+    :hover {        
+        border-color: ${({ disabled }) => disabled ? '#f1ecec' : '#B3B3B3'};
+    }
 `
