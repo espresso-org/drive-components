595da79a3fb7579e8dc533b0512f4061d671a7e4
Mathew Cormier <mathew.corm@gmail.com>
2018-09-25 09:06:18 -0400

    File list sidebar animation

--- src/components/side-bar.js
+++ src/components/side-bar.js
@@ -14,7 +14,7 @@ import { EditMode } from '../stores/edit-mode'
 export const SideBar = 
   inject("mainStore")( 
   ({ file, mainStore }) =>
-    <Main>
+    <Main visible={ file ? true : false }>
       <Tabs>Details</Tabs>
         
       {file &&
@@ -62,6 +62,8 @@ const Main = styled.aside`
   width: 300px;
   margin-left: 30px;
   min-height: 100%;
+  margin-right: ${({ visible }) => visible ? 0 : '-340px' };
+  transition: margin-right 300ms cubic-bezier(0.4,0.0,0.2,1);
 `
 const Tabs = styled.div`
   border-bottom: 1px solid ${theme.contentBorder};
